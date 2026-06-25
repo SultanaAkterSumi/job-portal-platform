2687259bb9922a93418bae5633402442e9daed9b
SultanaAkterSumi <sultanaakter4950@gmail.com>
2026-06-25 12:18:15 +0600

    Week 2: Add Job model and all job routes with filters

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import jobRoutes from "./routes/jobRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/jobs", jobRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
